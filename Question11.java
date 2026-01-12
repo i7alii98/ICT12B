803b80cd5bddac0e8c296ccec57aa3c64b6b9633
alii <98051488+i7alii98@users.noreply.github.com>
2026-01-12 19:42:37 +0300

    Delete Question11.java

--- Question11.java
+++
@@ -1,9 +0,0 @@
-public class StudentInfo {
-    public static void main(String[] args) {
-        int age = 25;
-        String name = "John";
-        double height = 5.9;
-
-        System.out.println("Age: " + age + ", Name: " + name + ", Height: " + height);
-    }
-}
