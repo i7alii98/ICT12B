e95fa4ce887661d793e6e27ce21cdc5eb6378745
alii <98051488+i7alii98@users.noreply.github.com>
2026-01-12 19:42:49 +0300

    Delete Question12.java

--- Question12.java
+++
@@ -1,8 +0,0 @@
-public class StudentPassCheck {
-    public static void main(String[] args) {
-        int score = 67; // change this value to test
-        boolean passed = score > 50;
-
-        System.out.println(score + ": " + passed);
-    }
-}
