7d8fae9d68894c06cf630befc592911665bc17d4
alii <98051488+i7alii98@users.noreply.github.com>
2026-01-12 19:42:22 +0300

    Delete Question 10.java

--- Question 10.java
+++
@@ -1,8 +0,0 @@
-public class PrintNameAge {
-    public static void main(String[] args) {
-        String name = "John";
-        int age = 25;
-
-        System.out.println("Name: " + name + ", Age: " + age);
-    }
-}
